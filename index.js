1c6049d9ab0ff3089213403bcd2fdc0c573a7064
Nin Alamo <nino.alamo@opteonsolutions.com>
2021-09-23 09:43:59 +0800

    change webhook url to my heroku url

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.get('/', function(req, response) {
         url: 'https://chatapi.viber.com/pa/set_webhook',
         headers: headerBody,
         body: {
-            url: 'https://susi-viberbot.tk/',
+            url: 'https://eoviberbot.herokuapp.com/',
             event_types: ['delivered', 'seen', 'failed', 'subscribed', 'unsubscribed', 'conversation_started']
         },
         json: true
