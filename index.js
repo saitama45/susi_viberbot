8a040efe9ba04c66b5a6123e427b25391f61252a
Nin Alamo <nino.alamo@opteonsolutions.com>
2021-09-24 16:56:09 +0800

    commit 25

--- index.js
+++ index.js
@@ -376,7 +376,7 @@ app.post('/', function(req, response) {
                     {
                         Columns: 6,
                         Rows: 2,
-                        Text: "<font color=#323232><b>Learn More</b></font>",
+                        Text: "<font color=#323232><b>Book Appointment</b></font>",
                         "ActionType":"open-url",
                         "ActionBody": "http://www.executiveoptical.com/Appointment",
                         "TextSize": "regular",
@@ -396,7 +396,7 @@ app.post('/', function(req, response) {
                     {
                         Columns: 6,
                         Rows: 2,
-                        Text: "<font color=#323232><b>Learn More</b></font>",
+                        Text: "<font color=#323232><b>Book Appointment</b></font>",
                         "ActionType":"open-url",
                         "ActionBody": "http://www.executiveoptical.com/Appointment",
                         "TextSize": "regular",
@@ -416,7 +416,7 @@ app.post('/', function(req, response) {
                     {
                         Columns: 6,
                         Rows: 2,
-                        Text: "<font color=#323232><b>Learn More</b></font>",
+                        Text: "<font color=#323232><b>Book Appointment</b></font>",
                         "ActionType":"open-url",
                         "ActionBody": "http://www.executiveoptical.com/Appointment",
                         "TextSize": "regular",
@@ -436,7 +436,7 @@ app.post('/', function(req, response) {
                     {
                         Columns: 6,
                         Rows: 2,
-                        Text: "<font color=#323232><b>Learn More</b></font>",
+                        Text: "<font color=#323232><b>Book Appointment</b></font>",
                         "ActionType":"open-url",
                         "ActionBody": "http://www.executiveoptical.com/Appointment",
                         "TextSize": "regular",
