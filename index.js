804b78174ae4a20c68ae0b17a01d07b1c8bff259
Nin Alamo <nino.alamo@opteonsolutions.com>
2021-09-24 15:02:38 +0800

    commit 25

--- index.js
+++ index.js
@@ -354,8 +354,8 @@ app.post('/', function(req, response) {
                         "TextHAlign": "middle"
                     },
                     {
-                        Columns: 6,
-                        Rows: 3,
+                        Columns: 5,
+                        Rows: 2,
                         Text: "<font color=#323232><b>Learn More</b></font>",
                         "ActionType":"reply",
                         "ActionBody": "Learn More in SM Southmall",
