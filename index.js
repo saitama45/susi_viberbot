0d8cd31cec013f7e8a7bae0187bea1afe4b2df6d
Nin Alamo <nino.alamo@opteonsolutions.com>
2021-09-27 14:56:13 +0800

    commit 25

--- index.js
+++ index.js
@@ -1006,7 +1006,7 @@ app.post('/', function(req, response) {
                         headers: headerBody,                        
                         body: {
                             receiver: req.body.sender.id,
-                            min_api_version: 6,
+                            min_api_version: 2,
                             type: 'rich_media',
                             keyboard: {
                                 Type: "rich_media",
